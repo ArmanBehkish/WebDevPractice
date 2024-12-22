f9968474eeed9aa710f95a88a7e41dd41bab4902
Arman Behkish <arman.behkish@gmail.com>
2024-12-22 15:28:10 +0100

    score board

---
+++ Basketball Scoreboard/index.js
@@ -0,0 +1,29 @@
+function homeAddOne() {
+    let homeScore = document.getElementById("home-score");
+    homeScore.textContent = parseInt(homeScore.textContent) + 1;
+}
+
+function homeAddTwo() {
+    let homeScore = document.getElementById("home-score");
+    homeScore.textContent = parseInt(homeScore.textContent) + 2;
+}
+
+function homeAddThree() {
+    let homeScore = document.getElementById("home-score");
+    homeScore.textContent = parseInt(homeScore.textContent) + 3;
+}
+
+function guestAddOne() {
+    let guestScore = document.getElementById("guest-score");
+    guestScore.textContent = parseInt(guestScore.textContent) + 1;
+}
+
+function guestAddTwo() {
+    let guestScore = document.getElementById("guest-score");
+    guestScore.textContent = parseInt(guestScore.textContent) + 2;
+}
+
+function guestAddThree() {
+    let guestScore = document.getElementById("guest-score");
+    guestScore.textContent = parseInt(guestScore.textContent) + 3;
+}
